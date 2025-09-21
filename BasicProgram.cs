2c852adcee1e5751bfd759b820c79874c9acc13e
Rootless <clementdud57@gmail.com>
2025-09-21 16:34:50 +0200

    Create BasicProgram.cs

---
+++ BasicProgram.cs
@@ -0,0 +1,9 @@
+using System;
+
+class Program {
+    static void Main() {
+        Console.WriteLine("Enter your name: ");
+        string name = Console.ReadLine();
+        Console.WriteLine($"Hello, {name}!");
+    }
+}
